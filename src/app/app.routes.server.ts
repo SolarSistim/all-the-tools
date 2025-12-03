f0bacbc9622d303f87ab5b5b0ad2fb9edf6cca13
SolarSistim <47832728+SolarSistim@users.noreply.github.com>
2025-12-03 14:32:16 -0600

    Fixing Netlify blog build error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { BLOG_ARTICLES_METADATA } from './features/blog/data/articles-metadata.data';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'blog/:slug',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      // Return all article slugs for prerendering
+      return BLOG_ARTICLES_METADATA.map(article => ({ slug: article.slug }));
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
