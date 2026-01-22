77bf7b54f2f8f778dc0a2269122b324b87ec9a68
SolarSistim <47832728+SolarSistim@users.noreply.github.com>
2026-01-22 11:05:17 -0600

    Added exploding trees article

--- src/app/features/blog/data/content/article-content-loader.ts
+++ src/app/features/blog/data/content/article-content-loader.ts
@@ -10,6 +10,10 @@ export async function loadArticleContent(
   try {
     // Explicit import mapping for each article
     switch (slug) {
+      case 'exploding-trees-in-winter-what-causes-the-loud-cracks':
+        const explodingTreesModule = await import('./exploding-trees-in-winter-what-causes-the-loud-cracks.content');
+        return explodingTreesModule.content;
+
       case 'masters-of-the-universe-2026-he-man-returns-to-the-big-screen':
         const heManModule = await import('./masters-of-the-universe-2026-he-man-returns-to-the-big-screen.content');
         return heManModule.content;
