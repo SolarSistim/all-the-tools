973c436e36c54351d8287b4ae10cb27bd4d1e022
SolarSistim <47832728+SolarSistim@users.noreply.github.com>
2026-01-23 12:11:11 -0600

    fixing social media launcher component

--- src/app/features/tools/social-media/social-launchpad/components/social-launchpad/social-launchpad.ts
+++ src/app/features/tools/social-media/social-launchpad/components/social-launchpad/social-launchpad.ts
@@ -351,7 +351,7 @@ export class SocialLaunchpadComponent implements OnInit, OnDestroy {
       this.hashtags.set([]);
       this.ogData.set(null);
       this.ogError.set(null);
-      this.selectedPlatforms.set([]);
+      this.selectedPlatforms.set(this.preferences.defaultPlatforms);
       this.cursorPosition.set(0);
 
       this.localStorageService.clearCurrentContent();
