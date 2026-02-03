45aa33a53663292428440c6f863ec7f67c38ae9f
SolarSistim <47832728+SolarSistim@users.noreply.github.com>
2026-02-03 16:53:19 -0600

    Added Roku lawsuit post

--- src/app/features/blog/data/content/article-content-loader.ts
+++ src/app/features/blog/data/content/article-content-loader.ts
@@ -10,6 +10,10 @@ export async function loadArticleContent(
   try {
     // Explicit import mapping for each article
     switch (slug) {
+      case 'roku-tv-black-screen-defect-class-action-lawsuit':
+        const rokuBlackScreenModule = await import('./roku-tv-black-screen-defect-class-action-lawsuit.content');
+        return rokuBlackScreenModule.content;
+
       case 'pensacola-mexican-food-trucks-guide':
         const foodTrucksModule = await import('./pensacola-food-trucks-complete-guide.content');
         return foodTrucksModule.content;
