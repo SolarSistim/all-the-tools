4c103fe021b28d07d79820b4f920c7d3666b1cab
SolarSistim <47832728+SolarSistim@users.noreply.github.com>
2026-02-23 06:22:51 -0600

    added localhost proxy config to access blog articles

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   ogFetchUserLockoutMinutes: 10,
   // Use deployed site URL for Identity API (localhost doesn't have Netlify Identity backend)
   netlifyIdentitySiteUrl: 'https://www.allthethings.dev',
-  newsUrl: 'https://json.allthethings.dev/news/news.json',
-  resourcesUrl: 'https://json.allthethings.dev/resources',
-  artistsUrl: 'https://json.allthethings.dev/3d-artist-spotlight',
-  blogUrl: 'https://json.allthethings.dev/blog'
+  newsUrl: '/json-proxy/news/news.json',
+  resourcesUrl: '/json-proxy/resources',
+  artistsUrl: '/json-proxy/3d-artist-spotlight',
+  blogUrl: '/json-proxy/blog'
 };
